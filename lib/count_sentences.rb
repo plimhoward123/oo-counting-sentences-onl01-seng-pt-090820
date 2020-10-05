286d9034cc7c04c884204e75248eb4a4694d3312
Plimon Howard V <plimon.howard@gmail.com>
2020-10-05 21:37:23 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+    return self.end_with?(".")
   end
 
   def question?
-
+    return self.end_with?("?")
   end
 
   def exclamation?
-
+    return self.end_with?("!")
   end
 
   def count_sentences
-
+    if (self.empty?)
+      return 0
+    end
+    tmparry = self.split(/[!?.]/)
+    tmparry.delete("")
+    return tmparry.count
   end
 end
